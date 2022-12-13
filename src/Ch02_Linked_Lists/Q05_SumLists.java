5896edc9f2502288efd488abc79ef2104d119686
AtakanDonmez <atk.donmez@gmail.com>
2022-12-13 21:47:17 +0300

    Q2.5

---
+++ src/Ch02_Linked_Lists/Q05_SumLists.java
@@ -0,0 +1,9 @@
+package Ch02_Linked_Lists;
+
+import CtCILibrary.LinkedListNode;
+
+public class Q05_SumLists {
+    public LinkedListNode addLists(LinkedListNode node1, LinkedListNode node2){
+
+    }
+}
