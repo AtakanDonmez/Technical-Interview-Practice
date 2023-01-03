434034b97ab2ab97c04e83679c0c919cbf6fc63a
AtakanDonmez <atk.donmez@gmail.com>
2023-01-03 19:15:47 +0300

    Q3.3

---
+++ src/Ch03_Stacks_and_Queues/Q03_StackOfPlates.java
@@ -0,0 +1,51 @@
+package Ch03_Stacks_and_Queues;
+
+import java.util.Stack;
+
+public class Q03_StackOfPlates {
+    public static class SetOfStacks {
+        private final int maxSize = 5;
+
+        private Stack<Stack<Integer>> set;
+        private int size;
+
+        public SetOfStacks(){
+            set = new Stack<Stack<Integer>>();
+            set.push(new Stack<Integer>());
+            size = 0;
+        }
+
+        public void push(Integer data){
+            if (set.peek().size() == maxSize){
+                Stack<Integer> newStack = new Stack<Integer>();
+                newStack.push(data);
+                set.push(newStack);
+            } else {
+                set.peek().push(data);
+            }
+            size++;
+        }
+
+        public Integer pop(){
+            if (size < 1) return null;
+
+            Integer data = set.peek().pop();
+            if (set.peek().isEmpty()){
+                set.pop();
+            }
+
+            size--;
+            return data;
+        }
+    }
+
+    public static void main(String[] args) {
+        SetOfStacks set = new SetOfStacks();
+        for (int i = 0; i < 34; i++) {
+            set.push(i);
+        }
+        for (int i = 0; i < 35; i++) {
+            System.out.println("Popped " + set.pop());
+        }
+    }
+}
