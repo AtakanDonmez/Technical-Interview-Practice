7f167efb913a6fcac685bd095f500678332db4cf
AtakanDonmez <atk.donmez@gmail.com>
2023-01-12 11:52:44 +0300

    Q8.3

---
+++ src/Ch08_Recursion_and_Dynamic_Programming/Q03_MagicIndex.java
@@ -0,0 +1,75 @@
+package Ch08_Recursion_and_Dynamic_Programming;
+
+import CtCILibrary.AssortedMethods;
+
+import java.util.Arrays;
+
+public class Q03_MagicIndex {
+    public static int magicSlow(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /* Creates an array that is sorted */
+    public static int[] getSortedArray(int size) {
+        int[] array = AssortedMethods.randomArray(size, -1 * size, size);
+        Arrays.sort(array);
+        return array;
+    }
+
+    /* Creates an array that is distinct and sorted */
+    public static int[] getDistinctSortedArray(int size) {
+        int[] array = AssortedMethods.randomArray(size, -1 * size, size);
+        Arrays.sort(array);
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == array[i-1]) {
+                array[i]++;
+            } else if (array[i] < array[i - 1]) {
+                array[i] = array[i-1] + 1;
+            }
+        }
+        return array;
+    }
+
+    public static int magicFast(int[] array){
+        return magicFast(array, 0, array.length - 1);
+    }
+
+    public static int magicFast(int[] array, int start, int end){
+        if (end < start) return -1;
+        if (start == end){
+            return (array[start] == start) ? start : -1;
+        }
+
+        int mid = (start + end) / 2;
+        if (array[mid] > mid){
+            return magicFast(array, start, mid - 1);
+        } else if (array[mid] < mid){
+            return magicFast(array, mid + 1, end);
+        } else {
+            return mid;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            int size = AssortedMethods.randomIntInRange(5, 20);
+            int[] array = getDistinctSortedArray(size);
+            int v2 = magicFast(array);
+            if (v2 == -1 && magicSlow(array) != -1) {
+                int v1 = magicSlow(array);
+                System.out.println("Incorrect value: index = -1, actual = " + v1 + " " + i);
+                System.out.println(AssortedMethods.arrayToString(array));
+                break;
+            } else if (v2 > -1 && array[v2] != v2) {
+                System.out.println("Incorrect values: index= " + v2 + ", value " + array[v2]);
+                System.out.println(AssortedMethods.arrayToString(array));
+                break;
+            }
+        }
+    }
+}
