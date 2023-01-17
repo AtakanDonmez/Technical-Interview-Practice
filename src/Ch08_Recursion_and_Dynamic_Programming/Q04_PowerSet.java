cedbc7138cf9fb115f56c8e92baf4b0819480f32
AtakanDonmez <atk.donmez@gmail.com>
2023-01-17 12:06:21 +0300

    Q10.1

--- src/Ch08_Recursion_and_Dynamic_Programming/Q04_PowerSet.java
+++ src/Ch08_Recursion_and_Dynamic_Programming/Q04_PowerSet.java
@@ -30,12 +30,13 @@ public class Q04_PowerSet {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i < 4; i++) {
             list.add(i);
         }
 
         ArrayList<ArrayList<Integer>> subsets2 = getSubsets(list);
         System.out.println(subsets2.toString());
+        System.out.println(subsets2.size());
     }
 
 }
